28565ce246561bc44ac76e1be49e4e2f226d255b
James Anderson <james.p.anderson16@gmail.com>
2017-07-29 11:43:45 -0600

    initial setup

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe User do
+  it "user can be created" do
+    user = User.create(email: "john_doe@mail.com",
+                       first_name: "John",
+                       last_name: "Doe",
+                       ss: "123-45-6789"
+                       )
+    expect(user).to be_valid
+  end
+
+  it "user cannot be created with missing field" do
+    user = User.create(email: "john_doe@mail.com",
+                       first_name: "John",
+                       last_name: "Doe"
+                       )
+    expect(user).to_not be_valid
+  end
+
+  it "user cannot be created without valid email address" do
+    user = User.create(email: "john_doe@mail",
+                       first_name: "John",
+                       last_name: "Doe",
+                       ss: "123-45-6789"
+                       )
+
+    expect(user).to_not be_valid
+  end
+end
